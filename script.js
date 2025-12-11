40ba0360c745961c810e7cc26ae5bae6eb03cc8a
Aditya Yedpalwar <100458076+Adityayedpalwar@users.noreply.github.com>
2025-12-11 12:45:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ const portfolioGrid = document.querySelector('.portfolio-grid');
 setInterval(() => {
     portfolioGrid.style.animation = 'scroll-portfolio 20s linear infinite';
 }, 20000);
+
+// FULLSCREEN FUNCTION
+function openFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) { 
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { 
+        element.msRequestFullscreen();
+    }
+}
+
+// Apply fullscreen on all images + videos
+document.querySelectorAll('.portfolio-item img, .portfolio-item video').forEach(item => {
+    item.addEventListener('click', () => openFullscreen(item));
+});
